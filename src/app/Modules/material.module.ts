efa6ef30d72140e7417ea881c665217c2a2d86a7
Yurii837 <gorogotskiy@gmail.com>
2024-01-20 14:47:51 +0200

    some fix

--- src/app/Modules/material.module.ts
+++ src/app/Modules/material.module.ts
@@ -1,12 +1,7 @@
 import { NgModule } from '@angular/core';
-import { MatTabsModule } from '@angular/material/tabs';
-import {MatSlideToggleModule} from '@angular/material/slide-toggle';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {MatSelectModule} from '@angular/material/select';
-import {MatTooltipModule} from '@angular/material/tooltip';
-import {MatDialogModule} from '@angular/material/dialog';
 import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
